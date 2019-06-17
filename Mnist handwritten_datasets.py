6ad60698bbace7df94519dd2c51e11f62b813144
shreyagarg31 <45915135+shreyagarg31@users.noreply.github.com>
2019-06-17 09:37:50 +0530

    Add files via upload

---
+++ Mnist handwritten_datasets.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import,division,print_function
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+mnist=tf.keras.datasets.mnist
+(train_images,train_labels),(test_images,test_labels)=mnist.load_data()
+train_images=train_images/255.0
+test_images=test_images/255.0
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(100,activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+model.fit(train_images,train_labels,epochs=5)
+test_loss,test_acc=model.evaluate(test_images,test_labels)
+print('test accuracy:',test_acc)
